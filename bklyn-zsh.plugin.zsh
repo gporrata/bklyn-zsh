e49e48d5d8b874f4805e508a277ab69a79d40bec
George Porrata <gporrata@gmail.com>
2017-02-13 20:38:34 +0100

    rebuild if not

--- bklyn-zsh.plugin.zsh
+++ bklyn-zsh.plugin.zsh
@@ -3,21 +3,26 @@ setopt NO_CHECK_JOBS
 
 bklyn_zsh_dir=${0:A:h}
 
-# if bklyn_zsh missing build it
-if [[ ! -f "${bklyn_zsh_dir}/bklyn_zsh" ]]; then
-  cargo build --release
-  cp ./target/release/bklyn_zsh .
-  rm -rf target
-fi
-
 # called prior to every command
 bklyn_zsh_precmd_hook() {
-  bklyn_zsh_exit_code=$? bklyn_zsh_pid=$$
-  PROMPT=`OSTYPE=$OSTYPE ${bklyn_zsh_dir}/bklyn_zsh -p zsh-left os dir git`
-  RPROMPT=`bklyn_zsh_pid=$bklyn_zsh_pid bklyn_zsh_exit_code=$bklyn_zsh_exit_code ${bklyn_zsh_dir}/bklyn_zsh -p zsh-right exit pid`
+  # if bklyn_zsh missing, build it
+  if [[ ! -f "${bklyn_zsh_dir}/bklyn_zsh" ]]; then
+    cargo build --release
+    cp ./target/release/bklyn_zsh .
+    rm -rf target
+  fi
+
+  # reinstall bklyn_zsh_precmd_hook
+  bklyn_zsh_precmd_hook() {
+    bklyn_zsh_exit_code=$? bklyn_zsh_pid=$$
+    PROMPT=`OSTYPE=$OSTYPE ${bklyn_zsh_dir}/bklyn_zsh -p zsh-left os dir git`
+    RPROMPT=`bklyn_zsh_pid=$bklyn_zsh_pid bklyn_zsh_exit_code=$bklyn_zsh_exit_code ${bklyn_zsh_dir}/bklyn_zsh -p zsh-right exit pid`
+  }
+
+  bklyn_zsh_precmd_hook()
 }
 
-# install bklyn_zsh
+# install bklyn_zsh hook
 bklyn_zsh_install() {
   # make sure to install bklyn-zsh only once
   if [[ $bklyn_zsh_installed != 'installed' ]]; then
