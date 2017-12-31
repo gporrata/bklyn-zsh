f5aa21c96440d3e1ad001271704515f2dcacb86d
gporrata <gporrata@gmail.com>
2017-12-31 15:45:38 -0500

    Initial tmux styling for the center

--- bklyn-zsh.tmux.zsh
+++ bklyn-zsh.tmux.zsh
@@ -2,12 +2,14 @@
 
 bklyn_zsh_dir=${0:A:h}
 
-for f in ostype; do
+for f in color_codes ostype; do
   source $bklyn_zsh_dir/src/$f.zsh
 done
 
 left_sep=$'\ue0b4'
 left_sep_thin=$'\ue0b5'
+
+center_color='#[fg=#ffffff]'
 
 right_color='#[bg=#222222]'
 right_sep='#[fg=#222222]'$'\ue0b6'
@@ -18,13 +20,29 @@ uptime_color='#[fg=#90caf9]'
 time_color='#[fg=#ffeb3b]'
 time_icon=$'\uf43a'
 
+center_debug() {
+    shift
+    echo "$center_color$@"
+}
+
+eval_center() {
+}
+
 case "$1" in
   left)
     echo " #[fg=$bklyn_zsh_ostype_color_code]$bklyn_zsh_ostype "
+    ;;
+  center)
+    shift
+    center_debug $@
+    #eval_center $@ 
     ;;
   right)
     right_repl="$right_sep$right_color$load_color$load_icon \4 \5 \6 $time_color$time_icon \1"
     right_line=`uptime | sed -nE "s/([^ ]+)[ ]+(([^,]+)[ ]+)?[0-9:]+,[ ]+[^,]+,[ ]+load average: ([^, ]+), ([^, ]+), ([^, ]+)/$right_repl/ p"`
     echo $right_line
+    ;;
+  *)
+    echo "Unknown specifier $1"
     ;;
 esac
